4f6b7ade3be280c199ad9eb1755c8ae2f1d73fb1
Rupem38 <40014564+rupeshmahadik@users.noreply.github.com>
2025-03-20 14:51:58 +0530

    changes moved from main to feature/mfone branch

---
+++ blocks/HomePageSwiper/HomePageSwiper.js
@@ -0,0 +1,69 @@
+import SwiperHomeOne from "../HomePageSwiper/swiper-bundle.min.js"
+
+export default function decorate(block) {
+    console.log("Heyyyy", block.children)
+    const SwiperWrapper = document.createElement('div')
+    SwiperWrapper.classList.add('swiper-wrapper')
+    Array.from(block.children).forEach(function (row, index) {
+        row.classList.add('swiper-slide')
+        SwiperWrapper.append(row)
+    })
+    const Swiper = document.createElement('div');
+    Swiper.classList.add('swiper');
+    Swiper.append(SwiperWrapper)
+    block.append(Swiper);
+
+    // Swipper Pagination
+    // const SwiperPagination = document.createElement('div')
+    // SwiperPagination.classList.add('swiper-pagination')
+    // block.append(SwiperPagination)
+
+    // Swipper Navigation Prev
+    // const SwipperNavBtnPrev = document.createElement('div')
+    // SwipperNavBtnPrev.classList.add('swiper-button-prev')
+    // block.append(SwipperNavBtnPrev)
+
+    // Swipper Navigation Next
+    // const SwipperNavBtnNext = document.createElement('div')
+    // SwipperNavBtnNext.classList.add('swiper-button-next')
+    // block.append(SwipperNavBtnNext)
+
+    // Swipper ScrollBar
+    // const SwipperScrollBar = document.createElement('div')
+    // SwipperScrollBar.classList.add('swiper-scrollbar')
+    // block.append(SwipperScrollBar)
+
+
+    SwiperHomeOne(block, {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay: true,
+
+        // If we need pagination
+        // pagination: {
+        //     el: SwiperPagination,
+        //     clickable: true
+        // },
+
+        // Navigation arrows
+        // navigation: {
+        //     nextEl: SwipperNavBtnNext,
+        //     prevEl: SwipperNavBtnPrev,
+        // },
+
+        // And if we need scrollbar
+        // scrollbar: {
+        //     el: SwipperScrollBar,
+        // },
+    })
+
+
+}
+
+
+// Swiper Wrapper
+// const swiper = document.createElement('div')
+// swiper.classList.add('swiper')
+// document.getElementsByClassName('homepageswiper')[0].classList.add('swiper')
+// const swiper = document.getElementsByClassName('swiper')[0]
---
+++ blocks/Rupesh/rupesh.js
@@ -0,0 +1,16 @@
+export default function decorate(block) {
+    console.log(block.children)
+    block.classList.add('my-class')
+    for (let i = 0; i < block.children.length; i++) {
+        block.children[i].classList.add('row-' + (i + 1));
+        for (let j = 0; j < block.children[i].children.length; j++) {
+            block.children[i].children[j].classList.add('row-' + (i + 1) + '-col-' + (j + 1))
+        }
+    }
+
+
+}
+
+
+
+
